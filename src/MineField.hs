c0df242b6f5ddd593ee61986e79423a03ae5c47d
gottagofaster236 <lr.soft.now@gmail.com>
2022-12-06 02:48:19 +0100

    open & flag cell

--- src/MineField.hs
+++ src/MineField.hs
@@ -1,23 +1,27 @@
-module MineField(CellState(..), Cell(..), MineField(..), GameState(..), MineGame(..),
-    generateMineField, openField, markField, countMinesLeft) where
+module MineField(CellState(..), Cell(..), MineField(..), GameState(..),
+    width, height, generateMineField, openCell, flagCell, countMinesLeft) where
 import GHC.Arr
 import Control.Monad.Random
 import System.Random.Shuffle
+import qualified Data.Set as Set
 
 data CellState = Unopened | Opened | Flagged
-    deriving (Eq)
+    deriving (Show, Eq)
 
 data Cell = Cell { cellState :: CellState, isMine :: Bool }
-    deriving (Eq)
+    deriving (Show, Eq)
 
-data MineField = MineField { width :: Int, height :: Int, cells :: Array (Int, Int) Cell }
-    deriving (Eq)
+newtype MineField = MineField { cells :: Array (Int, Int) Cell }
+    deriving (Show, Eq)
 
-data GameState = NotStarted | Running | Won | Lost
-    deriving (Eq)
+data GameState = Running | Won | Lost
+    deriving (Show, Eq)
 
-data MineGame = MineGame MineField GameState
-    deriving (Eq)
+width :: MineField -> Int
+width field = 1 + fst (snd $ bounds $ cells field)
+
+height :: MineField -> Int
+height field = 1 + snd (snd $ bounds $ cells field)
 
 -- Takes in the size (w, h) of the field and the number of mines.
 generateMineField :: (RandomGen g) => (Int, Int) -> Int -> Rand g MineField
@@ -29,13 +33,54 @@ generateMineField (fieldWidth, fieldHeight) minesCount = do
                                                                          j <- [0..fieldHeight - 1]]
     let isMine2DArray = array ((0, 0), (fieldWidth - 1, fieldHeight - 1)) isMine2DList
     let cells2DArray = amap (\isCellMine -> Cell { cellState = Unopened, isMine = isCellMine }) isMine2DArray
-    return MineField { width = fieldWidth, height = fieldHeight, cells = cells2DArray }
+    return MineField { cells = cells2DArray }
 
-openField :: MineField -> (Int, Int) -> MineField
-openField field _ = field
+openCell :: MineField -> (Int, Int) -> MineField
+openCell field position | cellState (cells field!position) /= Unopened = field 
+-- We opened a mine cell. Show the whole field.
+openCell field position | isMine (cells field!position) = do
+        field {cells = amap openCellIfNotOpened (cells field) }
+    where
+        openCellIfNotOpened :: Cell -> Cell
+        openCellIfNotOpened cell | cellState cell == Unopened = cell { cellState = Opened }
+        openCellIfNotOpened cell = cell
+-- We opened an ordinary cell.
+openCell field position = do
+        let fieldCells = cells field
+        let positionsToOpen = Set.toList $ findPositionsToOpen position Set.empty
+        let modifiedPositionsAndCells =
+             map (\modifiedPosition -> 
+                (modifiedPosition, (cells field!modifiedPosition){ cellState = Opened }))
+             positionsToOpen
+        field { cells = fieldCells//modifiedPositionsAndCells }
+    where
+        -- Takes in the field, the current position, and the accumulator.
+        findPositionsToOpen :: (Int, Int) -> Set.Set (Int, Int) -> Set.Set (Int, Int)
+        findPositionsToOpen (x, y) acc | x < 0 || x >= width field || y < 0 || y >= height field = acc
+        findPositionsToOpen currentPosition acc | cellState (cells field!currentPosition) /= Unopened = acc
+        findPositionsToOpen currentPosition acc | isMine (cells field!currentPosition) = acc
+        findPositionsToOpen currentPosition acc | Set.member currentPosition acc = acc
+        findPositionsToOpen (x, y) acc = do
+            let acc1 = Set.insert (x, y) acc
+            let acc2 = findPositionsToOpen (x - 1, y) acc1
+            let acc3 = findPositionsToOpen (x + 1, y) acc2
+            let acc4 = findPositionsToOpen (x, y - 1) acc3
+            let acc5 = findPositionsToOpen (x, y + 1) acc4
+            acc5
 
-markField :: MineField -> (Int, Int) -> MineField
-markField field _ = field
+
+flagCell :: MineField -> (Int, Int) -> MineField
+flagCell field position = do
+    let fieldCells = cells field
+    let oldCell = fieldCells!position
+    let newCells = if cellState oldCell == Unopened then
+            fieldCells//[(position, oldCell { cellState = Flagged })]
+        else
+            fieldCells
+    field { cells = newCells }
 
 countMinesLeft :: MineField -> Int
 countMinesLeft _ = 0
+
+getGameState :: MineField -> GameState
+getGameState _ = Running
--- test/test/MineField.hs
+++ test/test/MineField.hs
@@ -1,10 +1,11 @@
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
-{-# OPTIONS_GHC -Wmissing-export-lists #-}
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
 module Test.MineField where
 
 import MineField
 import Test.Tasty.HUnit
 import Control.Monad.Random
+    ( Rand, RandomGen, evalRand, mkStdGen, StdGen )
 import GHC.Arr
 
 gen :: StdGen
@@ -16,7 +17,7 @@ unit_generateMineFieldHasCorrectMinesCount = do
         ((0, 0), (49, 99)) @=? bounds mineCells
         50 @=? width mineField
         100 @=? height mineField
-        let countMines = sum [boolToInt $ isMine $ (cells mineField)!(i, j) | i <- [0..49], j <- [0..99]]
+        let countMines = sum [boolToInt $ isMine $ cells mineField!(i, j) | i <- [0..49], j <- [0..99]]
         30 @=? countMines
     where
         boolToInt :: Bool -> Int
@@ -24,7 +25,7 @@ unit_generateMineFieldHasCorrectMinesCount = do
 
 unit_generateMineFieldHasUnopenedFields = do
     let mineField = evalRand (generateMineField (123, 321) 7) gen
-    let allFieldsUnopened = all (\cell -> cellState cell == Unopened) (elems $ cells $ mineField)
+    let allFieldsUnopened = all (\cell -> cellState cell == Unopened) (elems $ cells mineField)
     assertBool "Fields should be unopened in the beginning" allFieldsUnopened
 
 unit_generateMineFieldHasDifferentResults = do
@@ -35,3 +36,132 @@ unit_generateMineFieldHasDifferentResults = do
             mineField1 <- generateMineField (500, 500) 500
             mineField2 <- generateMineField (500, 500) 500
             return $ mineField1 /= mineField2
+
+unit_openCellOneByOne = do
+    assertBool "Field should be unopened in the beginning" (cellState (cells oneByOneField!(0, 0)) == Unopened)
+    let openedField = openCell oneByOneField (0, 0)
+    Opened @=? cellState (cells openedField!(0, 0))
+
+unit_openSeveralCells = do
+    expectedOpenResult1 @=? openCell smallField1 (0, 1)
+    expectedOpenResult2 @=? openCell smallField2 (0, 1)
+    expectedOpenResult3 @=? openCell smallField3 (0, 1)
+
+unit_flagCell = do
+    Unopened @=? cellState (cells defaultField!(2, 3))
+    let flaggedField = flagCell defaultField (2, 3)
+    Flagged @=? cellState (cells flaggedField!(2, 3))
+    Unopened @=? cellState (cells flaggedField!(2, 4))
+
+unit_openFlaggedCell = do
+    let flaggedField = flagCell defaultField (5, 5)
+    Flagged @=? cellState (cells flaggedField!(5, 5))
+    let openedFlaggedField = openCell flaggedField (5, 5)
+    flaggedField @=? openedFlaggedField
+
+unit_flagOpenedCell = do
+    let openedField = openCell oneByOneField (0, 0)
+    Opened @=? cellState (cells openedField!(0, 0))
+    let flaggedOpenedField = flagCell openedField (0, 0)
+    openedField @=? flaggedOpenedField
+
+unit_openCellWithMine = do
+    expectedMineOpenResult1 @=? openCell smallField1 (1, 1)
+    expectedMineOpenResult2 @=? openCell smallField2 (1, 1)
+
+defaultField :: MineField
+defaultField = evalRand (generateMineField (10, 10) 10) gen
+
+
+oneByOneField :: MineField
+oneByOneField = MineField { cells = array ((0, 0), (1 - 1, 1 - 1)) [
+        ((0, 0), Cell { cellState = Unopened, isMine = False})
+    ]
+}
+
+-- 00
+-- 0X
+smallField1 :: MineField
+smallField1 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Unopened, isMine = False}),
+        ((0, 1), Cell { cellState = Unopened, isMine = False}),
+        ((1, 0), Cell { cellState = Unopened, isMine = False}),
+        ((1, 1), Cell { cellState = Unopened, isMine = True})
+    ]
+}
+
+-- __
+-- _X
+expectedOpenResult1 :: MineField
+expectedOpenResult1 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Opened, isMine = False}),
+        ((0, 1), Cell { cellState = Opened, isMine = False}),
+        ((1, 0), Cell { cellState = Opened, isMine = False}),
+        ((1, 1), Cell { cellState = Unopened, isMine = True})
+    ]
+}
+
+-- __
+-- _💥
+expectedMineOpenResult1 :: MineField
+expectedMineOpenResult1 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Opened, isMine = False}),
+        ((0, 1), Cell { cellState = Opened, isMine = False}),
+        ((1, 0), Cell { cellState = Opened, isMine = False}),
+        ((1, 1), Cell { cellState = Opened, isMine = True})
+    ]
+}
+
+-- 0F
+-- 0X
+smallField2 :: MineField
+smallField2 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Unopened, isMine = False}),
+        ((0, 1), Cell { cellState = Unopened, isMine = False}),
+        ((1, 0), Cell { cellState = Flagged, isMine = False}),
+        ((1, 1), Cell { cellState = Unopened, isMine = True})
+    ]
+}
+
+-- _F
+-- _X
+expectedOpenResult2 :: MineField
+expectedOpenResult2 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Opened, isMine = False}),
+        ((0, 1), Cell { cellState = Opened, isMine = False}),
+        ((1, 0), Cell { cellState = Flagged, isMine = False}),
+        ((1, 1), Cell { cellState = Unopened, isMine = True})
+    ]
+}
+
+-- _F
+-- _💥
+expectedMineOpenResult2 :: MineField
+expectedMineOpenResult2 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Opened, isMine = False}),
+        ((0, 1), Cell { cellState = Opened, isMine = False}),
+        ((1, 0), Cell { cellState = Flagged, isMine = False}),
+        ((1, 1), Cell { cellState = Opened, isMine = True})
+    ]
+}
+
+-- X0
+-- 0X
+smallField3 :: MineField
+smallField3 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Unopened, isMine = True}),
+        ((0, 1), Cell { cellState = Unopened, isMine = False}),
+        ((1, 0), Cell { cellState = Unopened, isMine = False}),
+        ((1, 1), Cell { cellState = Unopened, isMine = True})
+    ]
+}
+
+-- X0
+-- _X
+expectedOpenResult3 = MineField { cells = array ((0, 0), (2 - 1, 2 - 1)) [
+        ((0, 0), Cell { cellState = Unopened, isMine = True}),
+        ((0, 1), Cell { cellState = Opened, isMine = False}),
+        ((1, 0), Cell { cellState = Unopened, isMine = False}),
+        ((1, 1), Cell { cellState = Unopened, isMine = True})
+    ]
+}
